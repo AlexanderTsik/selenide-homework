05b91331d546e59d2ed2cab6d768fa54f07f5a5a
S0UL <93375971+AlexanderTsik@users.noreply.github.com>
2024-12-07 19:01:43 +0400

    Update Constants.java in assertions branch

--- src/main/java/ge/tbcitacademy/data/Constants.java
+++ src/main/java/ge/tbcitacademy/data/Constants.java
@@ -12,4 +12,5 @@ public class Constants {
     public static final String QUANTITY_NUM = "2";
 
     public static final String EXPECTED_COLOR = "rgba(40, 46, 137, 0.75)";
+    public static final String HOTFIX_CONSTANT = "Assertions Branch Constant";
 }
